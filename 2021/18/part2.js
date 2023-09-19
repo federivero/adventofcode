db187a5f962712b382ceb33c849a7dd86c1cfbe9
Federico Rivero <federico.rivero@pxlp.dev>
2023-09-19 10:44:09 -0300

    Day 18, 2021, js file

--- 2021/18/part2.js
+++ 2021/18/part2.js
@@ -1,206 +1,305 @@
+const data = `[[[[2,5],4],[[1,0],[8,3]]],[[2,[2,4]],[1,[3,3]]]]
+[[[2,2],[[4,3],3]],[[[8,6],3],[3,7]]]
+[[[9,[4,1]],[9,0]],[6,[6,0]]]
+[[[3,9],[[4,4],[2,5]]],[[9,[8,4]],8]]
+[[[[0,0],9],[[9,3],[8,2]]],[2,[1,3]]]
+[[[8,4],6],[[5,1],[3,6]]]
+[[[6,[7,6]],[[2,6],5]],[[6,4],2]]
+[[1,[9,7]],[[[5,9],[9,5]],[[7,0],1]]]
+[[[[5,8],[9,4]],[[9,3],[7,8]]],8]
+[[[0,9],[[6,0],7]],[[[7,7],6],[[9,7],[0,4]]]]
+[[[[4,3],[9,5]],[7,[7,3]]],[[[2,8],9],4]]
+[[7,5],[8,1]]
+[[4,6],[[[0,6],6],[7,4]]]
+[[[1,8],[[1,4],[1,6]]],[3,4]]
+[[[6,5],[4,[7,3]]],[[[0,1],[8,4]],[4,8]]]
+[[5,1],[9,[9,[3,3]]]]
+[[[[7,0],[2,5]],1],[9,[[2,7],[4,4]]]]
+[[[[5,8],8],0],[8,[1,[2,5]]]]
+[8,[[5,4],7]]
+[[[9,8],[6,7]],[[2,[2,6]],[9,6]]]
+[[[[2,3],7],6],[[8,6],3]]
+[[[8,[7,2]],3],[[[3,9],4],[6,8]]]
+[9,[[[6,7],[6,0]],[[3,9],8]]]
+[[[7,7],[4,7]],[[[9,8],9],[9,[2,4]]]]
+[[[[5,0],1],[4,[4,8]]],[9,[6,7]]]
+[[[[9,2],5],[1,[5,8]]],[[9,[0,1]],[3,8]]]
+[[[5,[2,5]],8],[2,[0,[9,3]]]]
+[[7,[[8,4],[8,4]]],4]
+[[[[3,3],4],[[0,0],[5,5]]],[4,5]]
+[[[[9,3],[9,3]],2],[5,3]]
+[[[9,5],[1,4]],[[7,1],[3,[6,5]]]]
+[8,[[[1,1],[0,1]],[9,[3,6]]]]
+[[[[4,4],7],[0,3]],[1,5]]
+[[[3,[0,8]],8],[5,[7,5]]]
+[[[[9,6],2],7],[[5,[3,7]],0]]
+[4,9]
+[[[5,[1,3]],[[9,5],6]],[[[7,9],5],3]]
+[[[[3,9],[7,2]],[5,[8,8]]],[1,9]]
+[[[[7,8],8],[[9,0],[5,1]]],[6,[[1,0],[3,3]]]]
+[[[[5,8],1],[[8,6],[2,9]]],[[5,1],6]]
+[[1,7],[[5,[3,2]],4]]
+[[[[3,1],2],[0,8]],[3,[4,6]]]
+[[9,6],[0,[[5,2],[1,1]]]]
+[[[[1,8],8],[[9,0],3]],[[6,[2,8]],[[6,4],[6,0]]]]
+[[7,[[3,2],[9,0]]],[[[3,2],[2,8]],[[5,5],[9,2]]]]
+[[[[2,5],[3,1]],[7,[9,6]]],[[[7,0],7],[2,[9,1]]]]
+[[[[1,6],9],[1,[6,5]]],[[8,[4,1]],6]]
+[[[7,[4,6]],[[2,7],[6,6]]],[8,0]]
+[[9,7],[[[0,7],5],[[1,4],[1,3]]]]
+[[[1,[8,2]],[[0,6],[9,0]]],8]
+[[[4,0],[7,[3,3]]],[9,6]]
+[0,[[[6,9],7],[[0,6],1]]]
+[5,[[4,3],[[8,3],[5,7]]]]
+[[9,0],[0,[[7,8],[1,8]]]]
+[[[[4,3],[5,6]],2],[[2,3],1]]
+[4,[[9,9],[[1,8],[9,2]]]]
+[[[[6,9],5],1],[[[7,4],[8,1]],3]]
+[[8,[5,[2,6]]],[[[2,7],6],[6,0]]]
+[[[[6,8],8],6],[[[5,7],2],[[6,5],[3,0]]]]
+[[[1,[2,5]],3],[5,[4,[6,6]]]]
+[[[[4,9],8],1],[9,0]]
+[[1,[0,[5,7]]],[[1,[5,9]],[[3,2],[1,7]]]]
+[[[[2,9],[2,7]],[[4,2],5]],[[[9,1],[7,2]],[2,[7,5]]]]
+[[[[5,7],[8,9]],[5,[7,9]]],[[7,[6,6]],[7,[8,0]]]]
+[[[[6,6],[4,6]],[4,[7,8]]],[1,[[5,5],[1,9]]]]
+[[[[4,3],8],2],[[9,[4,0]],[8,[7,0]]]]
+[[2,[7,5]],[[[0,1],1],[8,[3,5]]]]
+[[[4,[4,2]],[[0,4],9]],[1,4]]
+[[[5,5],[5,6]],[[0,[4,2]],[[7,8],[5,6]]]]
+[2,[[0,[9,1]],[[1,7],[0,0]]]]
+[[[5,[4,8]],1],9]
+[8,[[2,1],[3,0]]]
+[[[[6,5],[1,1]],7],[[[7,5],3],[0,1]]]
+[[[[0,3],7],7],[[[4,8],[6,1]],[[6,1],9]]]
+[[[[4,8],9],[1,0]],[6,[4,[4,8]]]]
+[[[[8,0],[5,1]],6],1]
+[[[[6,6],[7,7]],[[4,3],[2,6]]],[[3,5],[[7,0],[7,3]]]]
+[[1,[5,8]],[[[3,7],[9,6]],[[4,8],[3,4]]]]
+[[[1,5],[8,2]],[[[3,1],5],[4,1]]]
+[[[[6,3],5],8],[[9,[3,6]],[[3,5],[6,9]]]]
+[[[7,[5,4]],[0,[6,0]]],[[[7,7],[1,1]],[[5,1],7]]]
+[[[1,5],[[8,6],0]],5]
+[[[[0,8],[6,0]],[[3,0],9]],[[[7,1],2],[4,2]]]
+[[[6,[8,7]],[2,[2,0]]],[9,[7,[6,6]]]]
+[3,[[7,[4,5]],[[8,5],4]]]
+[[[[8,0],[8,3]],[[5,4],[1,6]]],[[0,[8,5]],3]]
+[[[7,2],1],[9,[[3,8],4]]]
+[[4,[7,[9,9]]],[3,8]]
+[[[[7,1],9],[[6,9],[9,6]]],[2,0]]
+[[[[6,2],9],[3,[3,9]]],[[8,[3,4]],[3,7]]]
+[[4,9],[8,[5,[9,8]]]]
+[3,[[9,[9,7]],4]]
+[[[[5,9],6],[1,[3,1]]],[4,[1,[3,8]]]]
+[[[[7,6],2],3],[[0,[1,8]],[[4,9],[4,3]]]]
+[[3,[[8,1],[3,8]]],[[[2,0],[0,8]],[[7,0],9]]]
+[[[[9,7],[9,3]],[[5,8],6]],[[[6,2],0],[2,4]]]
+[[[8,[9,7]],[[5,1],[1,4]]],3]
+[[7,[[5,6],[2,7]]],[[[7,3],0],[1,[0,6]]]]
+[[2,[[5,5],2]],[[3,[7,2]],[[7,1],8]]]
+[[[[2,4],[6,8]],[0,[7,5]]],[[3,[2,5]],[7,7]]]`
 
-var fs = require('fs');
+const sample = `[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+[[[5,[2,8]],4],[5,[[9,9],0]]]
+[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+[[[[5,4],[7,7]],8],[[8,3],8]]
+[[9,3],[[9,9],[6,[4,9]]]]
+[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]`
 
-fs.readFile('input.txt', 'utf-8', (err, data) => {
+let lines = data.split("\n");
+let numbers = [];
+let currentNumber = null;
+for (let i = 0; i < lines.length; i++){
 
-	if (err){
-		console.log(err);
-		process.exit(1);
-	}
+	let line = lines[i];
 
-	let lines = data.split("\r\n");
-	//console.log(lines);
-	
-	let numbers = [];
-	let currentNumber = null;
-	for (let i = 0; i < lines.length; i++){
-
-		let line = lines[i];
-
-		let numberStack = [];
-		currentNumber = null;
-		for (let j = 0; j < line.length; j++){
-			let char = line[j];
-			if (char == "["){
-				if (currentNumber){
-					numberStack.push(currentNumber);
-				}
-				currentNumber = {};
-			}else if (char == "]"){
-				let parent = numberStack.pop();
-				if (parent){
-					if (parent.left != undefined){
-						parent.right = currentNumber;
-					}else{
-						parent.left = currentNumber;
-					}
-					currentNumber = parent;					
-				}
-			}else if (char != ","){
-				let num = parseInt(char);
-				if (currentNumber.left != undefined){
-					currentNumber.right = num;
+	let numberStack = [];
+	currentNumber = null;
+	for (let j = 0; j < line.length; j++){
+		let char = line[j];
+		if (char == "["){
+			if (currentNumber){
+				numberStack.push(currentNumber);
+			}
+			currentNumber = {};
+		}else if (char == "]"){
+			let parent = numberStack.pop();
+			if (parent){
+				if (parent.left != undefined){
+					parent.right = currentNumber;
 				}else{
-					currentNumber.left = num;
+					parent.left = currentNumber;
 				}
+				currentNumber = parent;					
 			}
-		}
-
-		numbers.push(currentNumber);
-	}
-
-	var isNumber = function(obj){
-		return !isNaN(obj);
-	}
-
-	var magnitude = function(number){
-		let magn = 0;
-		if (isNumber(number.left)){
-			magn += 3 * number.left;
-		}else{
-			magn += 3 * magnitude(number.left);
-		}
-		if (isNumber(number.right)){
-			magn += 2 * number.right;
-		}else{
-			magn += 2 * magnitude(number.right);
-		}
-		return magn;
-	}
-
-	var addSingle = function(target, value, direction){
-		if (direction == "left"){
-			if (isNumber(target.left)){
-				target.left = target.left + value;
+		}else if (char != ","){
+			let num = parseInt(char);
+			if (currentNumber.left != undefined){
+				currentNumber.right = num;
 			}else{
-				addSingle(target.left, value, direction);
-			}
-		}else if (direction == "right"){
-			if (isNumber(target.right)){
-				target.right = target.right + value;
-			}else{
-				addSingle(target.right, value, direction);
+				currentNumber.left = num;
 			}
 		}
 	}
-	var explode = function(number, nestings){
-		if (nestings == 4){
-			return { value: number, changeNumber: true, addLeft: true, addRight: true };
+
+	numbers.push(currentNumber);
+}
+
+var isNumber = function(obj){
+	return !isNaN(obj);
+}
+
+var magnitude = function(number){
+	let magn = 0;
+	if (isNumber(number.left)){
+		magn += 3 * number.left;
+	}else{
+		magn += 3 * magnitude(number.left);
+	}
+	if (isNumber(number.right)){
+		magn += 2 * number.right;
+	}else{
+		magn += 2 * magnitude(number.right);
+	}
+	return magn;
+}
+
+var addSingle = function(target, value, direction){
+	if (direction == "left"){
+		if (isNumber(target.left)){
+			target.left = target.left + value;
 		}else{
-			let result = null;
-			if (!isNumber(number.left)){
-				result = explode(number.left, nestings + 1);
+			addSingle(target.left, value, direction);
+		}
+	}else if (direction == "right"){
+		if (isNumber(target.right)){
+			target.right = target.right + value;
+		}else{
+			addSingle(target.right, value, direction);
+		}
+	}
+}
+var explode = function(number, nestings){
+	if (nestings == 4){
+		return { value: number, changeNumber: true, addLeft: true, addRight: true };
+	}else{
+		let result = null;
+		if (!isNumber(number.left)){
+			result = explode(number.left, nestings + 1);
+			if (result && result.changeNumber){
+				result.changeNumber = false;
+				number.left = 0;
+			}
+			if (result && result.addRight){
+				if (isNumber(number.right)){
+					number.right = number.right + result.value.right;
+				}else{
+					addSingle(number.right, result.value.right, "left");
+				}
+				result.addRight = false;
+			}
+		}
+		if (!result){
+			if (!isNumber(number.right)){
+				result = explode(number.right, nestings + 1);
 				if (result && result.changeNumber){
 					result.changeNumber = false;
-					number.left = 0;
+					number.right = 0;
 				}
-				if (result && result.addRight){
-					if (isNumber(number.right)){
-						number.right = number.right + result.value.right;
+				if (result && result.addLeft){
+					if (isNumber(number.left)){
+						number.left = number.left + result.value.left;
 					}else{
-						addSingle(number.right, result.value.right, "left");
+						addSingle(number.left, result.value.left, "right");
 					}
-					result.addRight = false;
+					result.addLeft = false;
 				}
 			}
-			if (!result){
-				if (!isNumber(number.right)){
-					result = explode(number.right, nestings + 1);
-					if (result && result.changeNumber){
-						result.changeNumber = false;
-						number.right = 0;
-					}
-					if (result && result.addLeft){
-						if (isNumber(number.left)){
-							number.left = number.left + result.value.left;
-						}else{
-							addSingle(number.left, result.value.left, "right");
-						}
-						result.addLeft = false;
-					}
-				}
-			}
-			return result;
 		}
+		return result;
 	}
+}
 
-	var split = function(number){
-		let splitted = false;
-		if (isNumber(number.left)){
-			if (number.left >= 10){
-				number.left = { left: Math.floor(number.left / 2), right: Math.ceil(number.left / 2) };
-				splitted = true;				
+var split = function(number){
+	let splitted = false;
+	if (isNumber(number.left)){
+		if (number.left >= 10){
+			number.left = { left: Math.floor(number.left / 2), right: Math.ceil(number.left / 2) };
+			splitted = true;				
+		}
+	}else{
+		splitted = split(number.left);
+	}
+	if (!splitted){
+		if (isNumber(number.right)){
+			if (number.right >= 10){
+				number.right = { left: Math.floor(number.right / 2), right: Math.ceil(number.right / 2) };
+				splitted = true;					
 			}
 		}else{
-			splitted = split(number.left);
-		}
-		if (!splitted){
-			if (isNumber(number.right)){
-				if (number.right >= 10){
-					number.right = { left: Math.floor(number.right / 2), right: Math.ceil(number.right / 2) };
-					splitted = true;					
-				}
-			}else{
-				splitted = split(number.right);
-			}	
-		}
-		return splitted;
+			splitted = split(number.right);
+		}	
 	}
+	return splitted;
+}
 
-	var reduce = function(number){
-		let finished = false;
-		//console.log("reduction");
-		while (!finished){
-			let exploded = explode(number, 0);
-			//console.log("after explode", stringify(number));
-			if (!exploded){
-				let splitted = split(number);
-				//console.log("after split", stringify(number));	
-				if (!splitted){
-					finished = true;
-				}
+var reduce = function(number){
+	let finished = false;
+	//console.log("reduction");
+	while (!finished){
+		let exploded = explode(number, 0);
+		//console.log("after explode", stringify(number));
+		if (!exploded){
+			let splitted = split(number);
+			//console.log("after split", stringify(number));	
+			if (!splitted){
+				finished = true;
 			}
 		}
+	}
+	return number;
+}
+
+var stringify = function(number){
+	let string = "";
+	if (isNumber(number)){
+		string = number;
+	}else{
+		string = "[" + stringify(number.left) + "," + stringify(number.right) + "]";
+	}
+	return string;
+}
+
+var clone = function(number){
+	if (isNumber(number)){
 		return number;
+	}else{
+		return { left: clone(number.left), right: clone(number.right) }; 
 	}
+}
 
-	var stringify = function(number){
-		let string = "";
-		if (isNumber(number)){
-			string = number;
-		}else{
-			string = "[" + stringify(number.left) + "," + stringify(number.right) + "]";
-		}
-		return string;
-	}
-
-	var clone = function(number){
-		if (isNumber(number)){
-			return number;
-		}else{
-			return { left: clone(number.left), right: clone(number.right) }; 
-		}
-	}
-
-	let maxMagnitude = 0;
-	for (let i = 0; i < numbers.length; i++){
-		let num1 = numbers[i];
-		for (let j = 0; j < numbers.length; j++){
-			if (i != j){
-				let num2 = numbers[j];
-				let sum = { left: clone(num1), right: clone(num2)};
-				let result = reduce(sum);
-				let mag = magnitude(result); 
-				maxMagnitude = Math.max(mag, maxMagnitude);
-				let sum2 = { left: clone(num2), right: clone(num1) };
-				result = reduce(sum2);
-				mag = magnitude(result); 
-				maxMagnitude = Math.max(mag, maxMagnitude);
-				console.log(i, j, maxMagnitude);
-			}
+let maxMagnitude = 0;
+for (let i = 0; i < numbers.length; i++){
+	let num1 = numbers[i];
+	for (let j = 0; j < numbers.length; j++){
+		if (i != j){
+			let num2 = numbers[j];
+			let sum = { left: clone(num1), right: clone(num2)};
+			let result = reduce(sum);
+			let mag = magnitude(result); 
+			maxMagnitude = Math.max(mag, maxMagnitude);
+			let sum2 = { left: clone(num2), right: clone(num1) };
+			result = reduce(sum2);
+			mag = magnitude(result); 
+			maxMagnitude = Math.max(mag, maxMagnitude);
+			// console.log(i, j, maxMagnitude);
 		}
 	}
+}
 
-	console.log(maxMagnitude);
-	console.log(numbers.length);
-});
+console.log(maxMagnitude);
+console.log(numbers.length);
